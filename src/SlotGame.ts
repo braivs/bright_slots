3bee516af898990a151dee401b748a6a7fb90075
Braivs <braivs@proton.me>
2026-01-14 13:00:43 +0700

    Create all reels

--- src/SlotGame.ts
+++ src/SlotGame.ts
@@ -72,8 +72,6 @@ export const SlotGame = (app: Application<Renderer>) => {
     app.stage.addChild(winText)
   }
 
-
-
   // Create reels
   const createReels = () => {
     const reelWidth = 150
@@ -86,66 +84,67 @@ export const SlotGame = (app: Application<Renderer>) => {
         (reelCount - 1) * spacing)) / 2
     const startY = 200
 
-    // Create only ONE reel (for simplify)
-    const reelContainer = new Container()
-    reelContainer.x = startX
-    reelContainer.y = startY
+    // Create ALL reels in loop
+    for (let i = 0; i < reelCount; i++) {
+      const reelContainer = new Container()
+      reelContainer.x = startX + i * (reelWidth + spacing) // Move next reel
+      reelContainer.y = startY
 
-    // Reel background
-    const reelBg = new Graphics()
-    reelBg.rect(0, 0, reelWidth, reelHeight)
-    reelBg.fill(0x2a2a3e) // dark blue
-    reelBg.stroke({width: 3, color: 0x4a4a5e}) // outline
-    reelContainer.addChild(reelBg)
+      // Reel background
+      const reelBg = new Graphics()
+      reelBg.rect(0, 0, reelWidth, reelHeight)
+      reelBg.fill(0x2a2a3e) // dark blue
+      reelBg.stroke({width: 3, color: 0x4a4a5e}) // outline
+      reelContainer.addChild(reelBg)
 
-    // mask (view area)
-    const mask = new Graphics()
-    mask.rect(0, 0, reelWidth, reelHeight)
-    mask.fill(0xffffff)
-    reelContainer.mask = mask
-    reelContainer.addChild(mask)
+      // mask (view area)
+      const mask = new Graphics()
+      mask.rect(0, 0, reelWidth, reelHeight)
+      mask.fill(0xffffff)
+      reelContainer.mask = mask
+      reelContainer.addChild(mask)
 
-    // Create one symbol
-    const createSymbol = () => {
-      const symbolText = new Text({
-        text: symbols[Math.floor(Math.random() * symbols.length)],
-        style: {
-          fontFamily: 'Arial',
-          fontSize: 80,
-          fill: '#FFFFFF'
-        }
+      // Create one symbol
+      const createSymbol = () => {
+        const symbolText = new Text({
+            text: symbols[Math.floor(Math.random() * symbols.length)],
+            style: {
+              fontFamily: 'Arial',
+              fontSize: 80,
+              fill: '#FFFFFF'
+            }
+          }
+        )
+
+        // center symbol horizontally
+        symbolText.anchor.set(0.5)
+        symbolText.x = reelWidth / 2 // reel center (150 / 2)
+
+        return symbolText
       }
-      )
 
-      // center symbol horizontally
-      symbolText.anchor.set(0.5)
-      symbolText.x = reelWidth / 2 // reel center (150 / 2)
+      // Symbols inside reel
+      // Create more symbols, then possible to see (for limitless scroll effect)
+      const reelSymbols: Text[] = []
+      for (let j = 0; j < symbolsPerReel + 2; j++) {
+        const symbol = createSymbol()
+        const symbolHeight = reelHeight / symbolsPerReel
+        symbol.y = j * symbolHeight // place symbols vertically
+        reelSymbols.push(symbol)
+        reelContainer.addChild(symbol)
+      }
 
-      return symbolText
+      // saving reel state
+      reels.push({
+        container: reelContainer,
+        symbols: reelSymbols,
+        spinning: false
+      })
+
+      // Add reel to stage
+      app.stage.addChild(reelContainer)
     }
-
-    // Symbols inside reel
-    // Create more symbols, then possible to see (for limitless scroll effect)
-    const reelSymbols: Text[] = []
-    for (let j = 0; j < symbolsPerReel + 2; j++) {
-      const symbol = createSymbol()
-      const symbolHeight = reelHeight / symbolsPerReel
-      symbol.y = j * symbolHeight // place symbols vertically
-      reelSymbols.push(symbol)
-      reelContainer.addChild(symbol)
-    }
-
-    // saving reel state
-    reels.push({
-      container: reelContainer,
-      symbols: reelSymbols,
-      spinning: false
-    })
-
-    // Add reel to stage
-    app.stage.addChild(reelContainer)
   }
-
 
   // Initialize game parts UI
   setupUI();
